76155008b3779f9804bacab26d5665b762f1f08b
Henrique <henrique,popi@gmail.com>
2021-03-16 21:33:39 -0300

    Arquivo JS refatorado adicionado

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path')
 module.exports = {
-  entry :['@babel/polyfill','whatwg-fetch', ''./js/script.js'],
+  entry :['@babel/polyfill','whatwg-fetch', './js/script.js'],
   output :{
     path:path.resolve(__dirname, './'),
     filename: 'main.js'
